41af51ca7fc1eecd3793cba1e5980bf694279ba6
andresalexanderpena-cpu <andres.alexander.pena@gmail.com>
2025-08-26 11:15:45 -0700

    Add files via upload

---
+++ StringObjects.java
@@ -0,0 +1,11 @@
+
+
+public class StringObjects {
+    public static void main(String[] args) {
+
+        String phrase = "Hello World";
+
+        System.out.println( phrase.substring(3,6));
+
+    }
+}
