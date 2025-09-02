b1fc37675e1e590cf2b5a968482ded5be69adeec
andresalexanderpena-cpu <andres.alexander.pena@gmail.com>
2025-09-02 11:35:09 -0700

    Add files via upload

---
+++ helloName.java
@@ -0,0 +1,14 @@
+public class helloName {
+    public static void main(String[] args) {
+
+        String name = "Andres";
+
+        helloThere(name);
+
+    }
+
+    static void helloThere(String name) {
+        System.out.println("Hello "+name);
+
+    }
+}
